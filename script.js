abccfae70b131b695146271cd455ac27fcbf6207
piekario13 <piekario13@gmail.com>
2018-10-13 23:04:57 +0200

    begin

---
+++ script.js
@@ -0,0 +1,11 @@
+// let add = document.getElementById('newButtonPlayer');
+// let list = document.getElementById('list');
+
+// add.addEventListener('click', function() {
+//     let element = document.createElement('li');
+//     element.innerHTML = 'Player' + list.getElementsByTagName('li').length;
+//     list.appendChild(element);
+//     console.log(element)
+    
+
+// })
